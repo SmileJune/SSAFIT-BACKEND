78a8d9128ee33968ca5fcf95aabd1d5eee48b0a2
SmileJun <godhkekf24@inha.edu>
2022-05-16 17:33:36 +0900

    Feat: "first commit"

---
+++ ssafit/src/main/java/com/ssafy/ssafit/model/dto/Video.java
@@ -0,0 +1,50 @@
+package com.ssafy.ssafit.model.dto;
+
+public class Video {
+	private int no;
+	private String title;
+	private String part;
+	private String channelName;
+	private String url;
+	private int viewCnt;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPart() {
+		return part;
+	}
+	public void setPart(String part) {
+		this.part = part;
+	}
+	public String getChannelName() {
+		return channelName;
+	}
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getViewCnt() {
+		return viewCnt;
+	}
+	public void setViewCnt(int viewCnt) {
+		this.viewCnt = viewCnt;
+	}
+	
+	
+}
+
