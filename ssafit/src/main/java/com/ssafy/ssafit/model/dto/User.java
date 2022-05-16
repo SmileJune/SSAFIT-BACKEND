0dff09f114e52b51cabf6fa9cab605270b758b25
SmileJun <godhkekf24@inha.edu>
2022-05-16 22:11:42 +0900

    Feat: "User dto 작성"

---
+++ ssafit/src/main/java/com/ssafy/ssafit/model/dto/User.java
@@ -0,0 +1,34 @@
+package com.ssafy.ssafit.model.dto;
+
+public class User {
+	private String id;
+	private String password;
+	private String nickname;
+	private String introduce;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getIntroduce() {
+		return introduce;
+	}
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+	
+	
+}
