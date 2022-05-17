6c2360cbb4bc7e10776df84d0541cb4afe133f97
SmileJun <godhkekf24@inha.edu>
2022-05-17 10:09:11 +0900

    Refactor: "사용자 정의 Exception을 모두 RuntimeException으로 변경"

--- ssafit/src/main/java/com/ssafy/ssafit/exception/UserNotFoundException.java
+++ ssafit/src/main/java/com/ssafy/ssafit/exception/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.ssafy.ssafit.exception;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
 	public UserNotFoundException() {
 		super("사용자를 찾을 수 없습니다.");
 	}
