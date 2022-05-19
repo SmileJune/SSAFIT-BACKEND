68a72e92dc8674a2183f07ecd95e726ef082535f
SmileJun <godhkekf24@inha.edu>
2022-05-19 14:35:55 +0900

    Feat: "searchVideo by Condition 기능 구현 "

--- ssafit/src/main/java/com/ssafy/ssafit/model/dao/VideoDao.java
+++ ssafit/src/main/java/com/ssafy/ssafit/model/dao/VideoDao.java
@@ -2,10 +2,12 @@ package com.ssafy.ssafit.model.dao;
 
 import java.util.List;
 
+import com.ssafy.ssafit.model.dto.Condition;
 import com.ssafy.ssafit.model.dto.Video;
 
 public interface VideoDao {
 	Video selectOne(int no);
 	List<Video> selectList();
 	List<Video> selectListByPart(int partNo);
+	List<Video> selectVideoByCondition(Condition condition);
 }
