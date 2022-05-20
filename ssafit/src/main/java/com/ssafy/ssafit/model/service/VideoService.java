1ee196b3ce85de9ede8ac475fea5219e4545b5a5
SmileJun <godhkekf24@inha.edu>
2022-05-20 11:13:18 +0900

    의미없음

--- ssafit/src/main/java/com/ssafy/ssafit/model/service/VideoService.java
+++ ssafit/src/main/java/com/ssafy/ssafit/model/service/VideoService.java
@@ -3,7 +3,6 @@ package com.ssafy.ssafit.model.service;
 import java.util.List;
 
 import com.ssafy.ssafit.model.dto.Condition;
-import com.ssafy.ssafit.model.dto.Plan;
 import com.ssafy.ssafit.model.dto.Video;
 
 public interface VideoService {
