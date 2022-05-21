e796151c4aa8df301b1a1f9ffee8f6e9919f232e
SmileJun <godhkekf24@inha.edu>
2022-05-21 21:54:58 +0900

    Fix : "updateUser메서드 Post방식에서 Put방식으로 변경 "

--- ssafit/src/main/java/com/ssafy/ssafit/controller/ApiUserController.java
+++ ssafit/src/main/java/com/ssafy/ssafit/controller/ApiUserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -81,7 +82,7 @@ public class ApiUserController {
 		return userService.getUser(JWTUtil.getUserIdByToken(token));
 	}
 	
-	@PostMapping("user/update")
+	@PutMapping("user/update")
 	public void updateUser(@RequestHeader("access-token") String token, @RequestBody User user) {
 		user.setId(JWTUtil.getUserIdByToken(token));
 		userService.updateUser(user);
