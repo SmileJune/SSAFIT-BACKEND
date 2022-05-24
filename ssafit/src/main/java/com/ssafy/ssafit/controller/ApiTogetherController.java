e89995bd905ad85bc1f6338e131dd4442b79a494
SmileJun <godhkekf24@inha.edu>
2022-05-24 12:17:36 +0900

    Feat "Together dto 생성 및 createTogether 기능 구현"

---
+++ ssafit/src/main/java/com/ssafy/ssafit/controller/ApiTogetherController.java
@@ -0,0 +1,29 @@
+package com.ssafy.ssafit.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.ssafit.model.dto.Together;
+import com.ssafy.ssafit.model.service.TogetherService;
+import com.ssafy.ssafit.model.service.TogetherServiceImpl;
+import com.ssafy.ssafit.util.JWTUtil;
+
+@RestController
+@RequestMapping("/api")
+public class ApiTogetherController {
+	
+	@Autowired
+	private TogetherServiceImpl TogetherService;
+	
+	@PostMapping("together/create")
+	public void create(@RequestHeader("access-token") String token, @RequestBody Together together) {
+		together.setUserId(JWTUtil.getUserIdByToken(token));
+		TogetherService.createTogether(together);
+	}
+	
+	
+}
---
+++ ssafit/src/main/java/com/ssafy/ssafit/model/service/TogetherServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ssafy.ssafit.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.ssafit.model.dao.TogetherDao;
+import com.ssafy.ssafit.model.dto.Together;
+
+@Service
+public class TogetherServiceImpl implements TogetherService {
+
+	@Autowired
+	private TogetherDao togetherDao;
+	
+	
+	@Override
+	public void createTogether(Together together) {
+		togetherDao.insertTogether(together);
+	}
+
+}
